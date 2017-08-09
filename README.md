86feb11e1274856fdd2595ead35ebd33f50cf0ae
laihuamin <1445509994@qq.com>
2017-08-09 13:33:08 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## webpack-domes 系列
+webpack在youtube上有一套很好的地址，可以看完那个之后，再来做着一系列的dome
+## dome01 —— entry file
+
---
+++ dome01/webpack.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    entry: './main.js',  //打包的路口文件
+    output: {
+        filename: 'bundle.js'
+    } //产出的文件
+}
