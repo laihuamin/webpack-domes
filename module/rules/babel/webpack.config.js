6e0ce2d42040fc978d6ad30e8a22e2725348590e
laihuamin <1445509994@qq.com>
2017-11-23 22:03:31 +0800

    babel config

--- module/rules/babel/webpack.config.js
+++ module/rules/babel/webpack.config.js
@@ -4,13 +4,20 @@ module.exports = {
     entry: './main.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        pathinfo: true
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
-                use: ['babel-loader']
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015']
+                    }
+                }
             }
         ]
     }
