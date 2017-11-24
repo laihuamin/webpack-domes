30a85492135785ae572f6f7a24e2abfd72a65d9b
laihuamin <1445509994@qq.com>
2017-11-24 23:41:18 +0800

    babel-loader

---
+++ module/rules/babel-loader/main.js
@@ -0,0 +1,10 @@
+document.write('Hello World');
+
+class A{
+    init(){
+        console.log(1);
+    }
+}
+
+const a = new A();
+a.init();
---
+++ module/rules/babel-loader/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: './main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        pathinfo: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015']
+                    }
+                }
+            }
+        ]
+    }
+}
